5ab71ecad38953f41d57335e366a0d813a2768fe
Kevin Wüstner <kevin.wuestner@sonnenberg.ch>
2025-08-14 15:07:46 +0200

    feat(assembler-tokenizer): add basic tokenizer

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,5 +1,24 @@
 use std::env;
+use std::path::Path;
+use std::fs;
 
-fn main() { println!("Hello, world!");
+mod tokenizer;
+
+fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filepath>", args[0]);
+        std::process::exit(1);
+    }
+    let file_path = &args[1];
+    let path = Path::new(file_path);
+    let file_stem = path
+        .file_stem()
+        .and_then(|s| s.to_str())
+        .unwrap_or_default()
+        .to_string();
+    let file_content = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let lines = file_content.lines().collect::<Vec<&str>>();
+
+    let tokenizedLines = tokenizer::tokenize(&lines);
 }
--- assembler/src/tokenizer.rs
+++ assembler/src/tokenizer.rs
@@ -1,52 +1,127 @@
-use std::io;
-
-pub enum TokenizerError {
-    IOError(io::Error)
+#[derive(Debug, PartialEq)]
+pub enum TokenizedLine {
+    AInstruction(String),
+    CInstruction(String),
+    Label(String),
 }
 
-pub enum AInstruction {
-    Number(u16),
-    Symbol(String)
+pub fn tokenize(source: &Vec<&str>) -> Vec<TokenizedLine> {
+    source
+        .iter()
+        .map(|line| strip_whitespaces(line))
+        .map(|line| strip_inline_comments(&line))
+        .filter(|line| !line.is_empty())
+        .map(|line| tokenize_line(&line))
+        .collect::<Vec<TokenizedLine>>()
 }
 
-bitflags::bitflags! {
-    pub struct DestMask: u8 {
-        const A=0b100;
-        const D=0b010;
-        const M=0b001;
+fn tokenize_line(line: &str) -> TokenizedLine {
+    if line.starts_with("@") {
+        TokenizedLine::AInstruction(line.to_string())
+    } else if line.starts_with("(") {
+        TokenizedLine::Label(line.to_string())
+    } else {
+        TokenizedLine::CInstruction(line.to_string())
     }
 }
 
-pub enum Jump {
-    Null,
-    JGT,
-    JEQ,
-    JGE,
-    JLT,
-    JNE,
-    JLE,
-    JMP
+fn strip_whitespaces(line: &str) -> String {
+    line
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect()
 }
 
-pub enum Comp {
-    pub mnemonic: &str;
-    pub command: u8;
+fn strip_inline_comments(line: &str) -> String {
+    let mut parts = line.split("//");
+    parts.nth(0).unwrap().to_string()
 }
 
-pub struct CInstruction {
-    pub dest: DestMask,
-    pub comp: Comp,
-    pub jump: Jump
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-pub enum Line {
-    A(AInstruction),
-    C(CInstruction),
-    Label(String),
-    Empty
-}
+    #[test]
+    fn strip_whitespaces_removes_whitespaces1() {
+        let input = " ";
+        let result = strip_whitespaces(input);
+        let expected = "";
+        assert_eq!(result, expected);
+    }
 
-pub fn scan(source: &str) -> Result<Vec<Line>, TokenizerError> {
-    vec!();
-}
+    #[test]
+    fn strip_whitespaces_removes_whitespaces2() {
+        let input = " @123";
+        let result = strip_whitespaces(input);
+        let expected = "@123";
+        assert_eq!(result, expected);
+    }
 
+    #[test]
+    fn strip_whitespaces_removes_whitespaces3() {
+        let input = " D = M + 1  ";
+        let result = strip_whitespaces(input);
+        let expected = "D=M+1";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn strip_inline_comment_removes_comments1() {
+        let input = "D=M+1//testtest";
+        let result = strip_inline_comments(input);
+        let expected = "D=M+1";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn strip_inline_comment_removes_comments2() {
+        let input = "//test";
+        let result = strip_inline_comments(input);
+        let expected = "";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn tokenize_line_should_return_a_instruction() {
+        let input = "@123";
+        let result = tokenize_line(input);
+        let expected = TokenizedLine::AInstruction(input.to_string());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn tokenize_line_should_return_label() {
+        let input = "(test)";
+        let result = tokenize_line(input);
+        let expected = TokenizedLine::Label(input.to_string());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn tokenize_line_should_return_c_instruction() {
+        let input = "D=M+1";
+        let result = tokenize_line(input);
+        let expected = TokenizedLine::CInstruction(input.to_string());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn tokenize_should_tokenize_string_vector() {
+        let input = vec!(
+            "   //test",
+            "@123  ",
+            "D= A +1",
+            "//something else",
+            "( Label )",
+            "M =1 // test"
+        );
+        let result = tokenize(&input);
+        let expected = vec!(
+            TokenizedLine::AInstruction("@123".to_string()),
+            TokenizedLine::CInstruction("D=A+1".to_string()),
+            TokenizedLine::Label("(Label)".to_string()),
+            TokenizedLine::CInstruction("M=1".to_string()),
+        );
+        assert_eq!(result, expected);
+    }
+}
